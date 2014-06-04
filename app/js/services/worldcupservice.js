76fdf47999e1d8285994c8023c6d508365e05aec
lesoto <lesoto@lesoto.com>
2014-06-04 01:05:33 -0400

    Added teams and flags - 007

--- app/js/services/worldcupservice.js
+++ app/js/services/worldcupservice.js
@@ -2,46 +2,51 @@ angular.module('mondial2014.services.worldcup', [])
   .factory('WorldCup', function() {
     var WorldCup = {};
     WorldCup.teams = [
-      {"abbr":"ARI", "team":"Arizona",        "mascot": "Cardinals",  "conference":"NFC", "division": "West"},
-      {"abbr":"ATL", "team":"Atlanta",        "mascot": "Falcons",    "conference":"NFC", "division": "South"},
-      {"abbr":"BAL", "team":"Baltimore",      "mascot": "Ravens",     "conference":"AFC", "division": "North"},
-      {"abbr":"BUF", "team":"Buffalo",        "mascot": "Bills",      "conference":"AFC", "division": "East"},
-      {"abbr":"CAR", "team":"Carolina",       "mascot": "Panthers",   "conference":"NFC", "division": "South"},
-      {"abbr":"CHI", "team":"Chicago",        "mascot": "Bears",      "conference":"NFC", "division": "North"},
-      {"abbr":"CIN", "team":"Cincinnati",     "mascot": "Bengals",    "conference":"AFC", "division": "North"},
-      {"abbr":"CLE", "team":"Cleveland",      "mascot": "Browns",     "conference":"AFC", "division": "North"},
-      {"abbr":"DAL", "team":"Dallas",         "mascot": "Cowboys",    "conference":"NFC", "division": "East"},
-      {"abbr":"DEN", "team":"Denver",         "mascot": "Broncos",    "conference":"AFC", "division": "West"},
-      {"abbr":"DET", "team":"Detroit",        "mascot": "Lions",      "conference":"NFC", "division": "North"},
-      {"abbr":"GB",  "team":"Green Bay",      "mascot": "Packers",    "conference":"NFC", "division": "North"},
-      {"abbr":"HOU", "team":"Houston",        "mascot": "Texans",     "conference":"AFC", "division": "South"},
-      {"abbr":"IND", "team":"Indianapolis",   "mascot": "Colts",      "conference":"AFC", "division": "South"},
-      {"abbr":"JAC", "team":"Jacksonville",   "mascot": "Jaguars",    "conference":"AFC", "division": "South"},
-      {"abbr":"KC",  "team":"Kansas City",    "mascot": "Chiefs",     "conference":"AFC", "division": "West"},
-      {"abbr":"MIA", "team":"Miami",          "mascot": "Dolphins",   "conference":"AFC", "division": "East"},
-      {"abbr":"MIN", "team":"Minnesota",      "mascot": "Vikings",    "conference":"NFC", "division": "North"},
-      {"abbr":"NE",  "team":"New England",    "mascot": "Patriots",   "conference":"AFC", "division": "East"},
-      {"abbr":"NO",  "team":"New Orleans",    "mascot": "Saints",     "conference":"NFC", "division": "South"},
-      {"abbr":"NYG", "team":"New York",       "mascot": "Giants",     "conference":"NFC", "division": "East"},
-      {"abbr":"NYJ", "team":"New York",       "mascot": "Jets",       "conference":"AFC", "division": "East"},
-      {"abbr":"OAK", "team":"Oakland",        "mascot": "Raiders",    "conference":"AFC", "division": "West"},
-      {"abbr":"PHI", "team":"Philadelphia",   "mascot": "Eagles",     "conference":"NFC", "division": "East"},
-      {"abbr":"PIT", "team":"Pittsburgh",     "mascot": "Steelers",   "conference":"AFC", "division": "North"},
-      {"abbr":"SD",  "team":"San Diego",      "mascot": "Chargers",   "conference":"AFC", "division": "West"},
-      {"abbr":"SEA", "team":"Seattle",        "mascot": "Seahawks",   "conference":"NFC", "division": "West"},
-      {"abbr":"SF",  "team":"San Francisco",  "mascot": "49ers",      "conference":"NFC", "division": "West"},
-      {"abbr":"STL", "team":"St. Louis",      "mascot": "Rams",       "conference":"NFC", "division": "West"},
-      {"abbr":"TB",  "team":"Tampa Bay",      "mascot": "Buccaneers", "conference":"NFC", "division": "South"},
-      {"abbr":"TEN", "team":"Tennessee",      "mascot": "Titans",     "conference":"AFC", "division": "South"},
-      {"abbr":"WAS", "team":"Washington",     "mascot": "Redskins",   "conference":"NFC", "division": "East"}
+      {  "teamId" : 31, "title" : "Russia", "code" : "rus", "group" : 8 }, 
+      {  "teamId" : 10, "title" : "Greece", "code" : "gre", "group" : 3 }, 
+      {  "teamId" : 6, "title" : "Netherlands", "code" : "ned", "group" : 2 }, 
+      {  "teamId" : 25, "title" : "Germany", "code" : "ger", "group" : 7 }, 
+      {  "teamId" : 26, "title" : "Portugal", "code" : "por", "group" : 7 }, 
+      {  "teamId" : 5, "title" : "Spain", "code" : "esp", "group" : 2 }, 
+      {  "teamId" : 16, "title" : "Italy", "code" : "ita", "group" : 4 }, 
+      {  "teamId" : 2, "title" : "Croatia", "code" : "cro", "group" : 1 }, 
+      {  "teamId" : 19, "title" : "France", "code" : "fra", "group" : 5 }, 
+      {  "teamId" : 15, "title" : "England", "code" : "eng", "group" : 4 }, 
+      {  "teamId" : 17, "title" : "Switzerland", "code" : "sui", "group" : 5 }, 
+      {  "teamId" : 29, "title" : "Belgium", "code" : "bel", "group" : 8 }, 
+      {  "teamId" : 22, "title" : "Bosnia-Herzegovina", "code" : "bih", "group" : 6 }, 
+      {  "teamId" : 30, "title" : "Algeria", "code" : "alg", "group" : 8 }, 
+      {  "teamId" : 11, "title" : "C\u00f4te d'Ivoire", "code" : "civ", "group" : 3 }, 
+      {  "teamId" : 27, "title" : "Ghana", "code" : "gha", "group" : 7 }, 
+      {  "teamId" : 4, "title" : "Cameroon", "code" : "cmr", "group" : 1 }, 
+      {  "teamId" : 24, "title" : "Nigeria", "code" : "nga", "group" : 6 }, 
+      {  "teamId" : 3, "title" : "Mexico", "code" : "mex", "group" : 1 }, 
+      {  "teamId" : 28, "title" : "United States", "code" : "usa", "group" :7 }, 
+      {  "teamId" : 20, "title" : "Honduras", "code" : "hon", "group" : 5 }, 
+      {  "teamId" : 14, "title" : "Costa Rica", "code" : "crc", "group" : 4 }, 
+      {  "teamId" : 21, "title" : "Argentina", "code" : "arg", "group" : 6 }, 
+      {  "teamId" : 1, "title" : "Brazil", "code" : "bra", "group" : 1 }, 
+      {  "teamId" : 7, "title" : "Chile", "code" : "chi", "group" : 2 }, 
+      {  "teamId" : 13, "title" : "Uruguay", "code" : "uru", "group" : 4 }, 
+      {  "teamId" : 9, "title" : "Colombia", "code" : "col", "group" : 3 }, 
+      {  "teamId" : 18, "title" : "Ecuador", "code" : "ecu", "group" : 5 }, 
+      {  "teamId" : 8, "title" : "Australia", "code" : "aus", "group" : 2 }, 
+      {  "teamId" : 12, "title" : "Japan", "code" : "jpn", "group" : 3 }, 
+      {  "teamId" : 32, "title" : "South Korea", "code" : "kor", "group" : 8 }, 
+      {  "teamId" : 23, "title" : "Iran", "code" : "irn", "group" : 6 }
     ];
-    WorldCup.positions = [
-      {"abbr":"QB",  "pos":"Quarterback"},
-      {"abbr":"RB",  "pos":"Runningback"},
-      {"abbr":"WR",  "pos":"Wide Receiver"},
-      {"abbr":"TE",  "pos":"Tight End"},
-      {"abbr":"K",   "pos":"Kicker"},
-      {"abbr":"D/ST","pos":"Defense/Special Teams"}
-    ];
-    return WorldCup;
+
+//    WorldCup.groups = [
+//      {"abbr":"Group A", { "teamName" : "Brazil"}, { "teamName" : "Croatia"}, { "teamName" : "Mexico"}, { "teamName" : "Cameroon"} },
+//      {"abbr":"Group B", { "teamName" : "Spain"}, { "teamName" : "Netherlands"}, { "teamName" : "Chile"}, { "teamName" : "Australia"} },
+//      {"abbr":"Group C", { "teamName" : "Colombia"}, { "teamName" : "Greece"}, { "teamName" : "Côte d''Ivoire  Japan"} },
+//      {"abbr":"Group D", { "teamName" : "Uruguay"}, { "teamName" : "Costa Rica"}, { "teamName" : "England"}, { "teamName" : "Italy"} },
+//      {"abbr":"Group E", { "teamName" : "Switzerland  Ecuador"}, { "teamName" : "France"}, { "teamName" : "Honduras"} },
+//      {"abbr":"Group F", { "teamName" : "Argentina"}, { "teamName" : "Bosnia-Herzegovina"}, { "teamName" : "Iran"}, { "teamName" : "Nigeria"} },
+//      {"abbr":"Group G", { "teamName" : "Germany"}, { "teamName" : "Portugal"}, { "teamName" : "Ghana"}, { "teamName" : "United States"} },
+//      {"abbr":"Group H", { "teamName" : "Belgium"}, { "teamName" : "Algeria"}, { "teamName" : "Russia"}, { "teamName" : "South Korea"} }
+//      ];
+    
+  return WorldCup;
+  
   });
