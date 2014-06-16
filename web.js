8a7b2f573ff99756d095da56d28e8c253ee98bfb
lesoto <lesoto@lesoto.com>
2014-06-16 00:16:34 -0400

    Added web.js routes - 015

--- web.js
+++ web.js
@@ -1,6 +1,4 @@
 var express = require("express");
-var path = require('path');
-var routes  = require('routes');
 var logfmt = require("logfmt");
 var bodyParser = require('body-parser');
 var gzippo = require('gzippo');
@@ -13,10 +11,19 @@ app.use(gzippo.staticGzip("" + __dirname + "/dist"));
  
 var port = Number(process.env.PORT || 5000);
 
-app.get('/signin', routes.signin);
-app.get('/signup', routes.signup);
-app.get('/teams', routes.teams);
-app.get('/teams/:TeamId', routes.teams);
+app.get('/signin', function (req, res) { res.render('signin'); } );
+//app.get('/signup', routes.signup);
+//app.get('/teams', routes.teams);
+//app.get('/teams/:TeamId', routes.teams);
+
+app.get('*', function(req, res) {
+  res.redirect('/#' + req.originalUrl);
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.send(500, { message: err.message });
+});
 
 app.listen(port, function() {
   console.log("Listening on " + port);
